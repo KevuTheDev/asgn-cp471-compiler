d63d515efc2ba149f448ad607d7a5e3ee85c450e
Kevin He <kevin66he@hotmail.com>
2023-04-10 19:16:16 -0400

    Using Compiler class to manage

--- compiler/src/Compiler.h
+++ compiler/src/Compiler.h
@@ -1,10 +1,41 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+
+#include "globals.h"
+
+#include "LogFileBuffer.h"
+#include "TokenFileBuffer.h"
+
+#include "ReservedWords.h"
+#include "SymbolTable.h"
+
+#include "Lexical.h"
+#include "Syntax.h"
+
 class Compiler
 {
 public:
-	Compiler();
+	Compiler(const std::string& filename);
+
+	void run();
+
 	~Compiler();
 
+private:
+	std::string _filename;
+
+	LogFileBuffer *_logfilebuffer;
+	TokenFileBuffer *_tokenfilebuffer;
+
+	ReservedWords *_reservedwords;
+	SymbolTable *_symboltable;
+	
+
+	Lexical *_lexical;
+
+	Syntax *_syntax;
 
 };
 
